b5f299c54f57222ab6d6d92804c27599912587b1
amalice <alice.amblard@outlook.fr>
2026-06-28 21:58:57 +0200

    s

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,5 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+console.log(process);
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
